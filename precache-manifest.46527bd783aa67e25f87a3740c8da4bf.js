22f305ac4ef2b6550f3a74e3531ba9c5fc65b401
hoa <hoanv.nta@gmail.com>
2022-05-17 08:58:27 +0700

    Updates

---
+++ precache-manifest.46527bd783aa67e25f87a3740c8da4bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "168a509e645e2ed699e891e62ec93dd8",
+    "url": "/react-redux/index.html"
+  },
+  {
+    "revision": "2350117c28ba671619e0",
+    "url": "/react-redux/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "158b2a4589e60bda54a8",
+    "url": "/react-redux/static/css/main.fecf9c79.chunk.css"
+  },
+  {
+    "revision": "2350117c28ba671619e0",
+    "url": "/react-redux/static/js/2.92ccf0e4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-redux/static/js/2.92ccf0e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "158b2a4589e60bda54a8",
+    "url": "/react-redux/static/js/main.bc2c116c.chunk.js"
+  },
+  {
+    "revision": "1bb70495f3fa5e6371d8",
+    "url": "/react-redux/static/js/runtime-main.b1df64bc.js"
+  }
+]);
